2fdf22d782614384e4a2698c626188e9e7290280
Augusto Pantaloni <AugustoMPantaloni@gmail.com>
2025-04-07 22:50:09 -0300

    PracticasC

---
+++ Avanzado/mario.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    printf("Height:\n");
+    scanf("%d", &height);
+
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= height - i; j++) {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= i; k++) {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
