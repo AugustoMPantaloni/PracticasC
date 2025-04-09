5eb3080bf1c0143080421245ae8840abc754f94c
Augusto Pantaloni <AugustoMPantaloni@gmail.com>
2025-04-08 22:35:03 -0300

    PracticasC

--- Avanzado/mario.c
+++ Avanzado/mario.c
@@ -6,12 +6,27 @@ int main(void)
     printf("Height:\n");
     scanf("%d", &height);
 
-    for (int i = 1; i <= height; i++) {
-        for (int j = 1; j <= height - i; j++) {
+    while( height <= 0)
+    {
+        printf("These are not valid values, please enter a positive integer.\n");
+        while(getchar() != '\n');
+        scanf("%d", &height);
+    }
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= height - i; j++)
+        {
             printf(" ");
         }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
 
-        for (int k = 1; k <= i; k++) {
+        printf(" ");
+
+        for(int k = 1; k <= i; k++ ){
             printf("#");
         }
 
