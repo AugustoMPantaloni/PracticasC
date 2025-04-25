7836843b95832b62dfbc0ae0d46eda543298bc2c
Augusto Pantaloni <AugustoMPantaloni@gmail.com>
2025-04-24 22:34:57 -0300

    PracticasC

--- Etapa5/A.c
+++ Etapa5/A.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
-int main(void){
+int main (void){
     int numeros[10];
 
-    printf("Ingrese 10 numeros enteros distintos:\n");
-
-    for(int i = 0; i < 10; i++){
+    printf("Se le pedira que ingrese 10 numeros enteros\n");
+    for (int i = 0; i < 10; i++){
+        printf("ingrese un numero\n");
         scanf("%d", &numeros[i]);
     }
 
-    int mayor = numeros[0];
-    int posicion = 0;
+    int mayor = numeros[0]; //Inicia la variable desde el primer numero del array por si el usuario ingresa todos numeros negativos
+    int posicion = 0; //
 
-    for(int indice = 1; indice < 10; indice++){
-        if(numeros[indice] > mayor){
-            mayor = numeros[indice];
-            posicion = indice;
+    for(int i = 1; i< 10; i++){ //El ciclo empieza desde indice = 1 porque indice = 0 ya se usa en la variable "mayor"
+        if (numeros[i] > mayor){
+            mayor = numeros[i];
+            posicion = i;
         }
     }
 
-    printf("El numero mas grande es: %d\n", mayor);
-    printf("La posicion en el array es la: %d\n", posicion);
+    printf("El numero mas grande es %d y se encuntra en la posicion %d del array", mayor, posicion);
 
     return 0;
 }
--- Etapa5/B.c
+++ Etapa5/B.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 
 int main(void){
-    char palabra[20];
 
-    printf("Ingrese una palabra:\n");
-    fgets(palabra, 20, stdin);
+    char palabra[50];
+
+    printf("Ingrese una palabra y se la devolvere invertida\n");
+    fgets(palabra, 50, stdin);
+    
+    for(int i = 0; i < 50; i++){
+        if(palabra[i] == '\n'){
+            palabra[i] = '\0';
+            break;
+        }
+    }
 
     int longitud;
-    for(longitud = 0;
-        palabra[longitud] != '\n'  && palabra[longitud] != '\0';
-        longitud++){}
-
-    char revez[20];
-    int indiceRevez = 0;
-
-    for(int i = longitud - 1; i >= 0; i--){
-        revez[indiceRevez] = palabra[i];
-        indiceRevez++;
+    for (int i = 0; i < 50; i++){
+        if(palabra[i] == '\0'){
+            longitud = i;
+            break;
+        }
     }
-    revez[indiceRevez] = '\0';
 
-    printf("La palabra al revez es: %s", revez);
+    char revez[longitud];
+    int j = 0; //indice para la palabra invertida
+    for(int i = longitud -1; i >= 0; i--){
+        revez[j] = palabra[i];
+        j++;
+    }
+
+    revez[j] = '\0';
+
+    printf("%s", revez);
+
+
+
     return 0;
 }
