7ecca38804bff430f976332477ef046cad216a9c
Augusto Pantaloni <AugustoMPantaloni@gmail.com>
2025-05-10 21:44:54 -0300

    Recursividad,Algoritmos,SubArrays

---
+++ SubArrays/1.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void subArray(int arr[], int n){
+    for(int start = 0; start < n; start++){
+        for( int end = start; end < n; end++){
+            for(int print = start; print <= end; print++){
+                printf("%d", arr[print]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+
+
+int main(int argc, char *argv[]) {
+    int arr[] = {3, 5, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    subArray(arr, n);
+
+    return 0;
+}
